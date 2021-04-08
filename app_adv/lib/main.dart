ac1cfb67edb95749e358327f5eb7ad3d85ea43dd
Márcio Gabriel <marciogabriel1998@gmail.com>
2021-04-07 21:28:14 -0300

    Adicionando pastas e arquivos base

--- app_adv/lib/main.dart
+++ app_adv/lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class PaginaInicial extends StatefulWidget {
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+}
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
